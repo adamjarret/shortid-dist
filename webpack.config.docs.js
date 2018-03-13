6cfa4935ba61d46de068f02db28df657d27a938d
Adam Jarret <adam@atj.me>
2018-03-12 16:00:09 -1000

    Added .nojekyll file to docs

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -1,16 +1,20 @@
 const HTMLPlugin = require('html-webpack-plugin')
+const TouchPlugin = require('webpack-touch');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+var outpath = __dirname + "/docs";
+
 module.exports = merge(common.cfg, {
   output: {
-    path: __dirname + "/docs"
+    path: outpath
   },
   plugins: [
     new HTMLPlugin({
         template: 'src/index.html',
         inject: 'head',
         cdnPrefix: 'https://unpkg.com/shortid-dist@' + common.version + '/dist/' + common.depVersion('shortid')
-    })
+    }),
+    new TouchPlugin({filename: outpath + '/.nojekyll', delay: 2000})
   ]
 });
