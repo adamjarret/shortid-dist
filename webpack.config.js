bed86b107db7d9def5f2447b404702c08993d36b
Adam Jarret <adam@atj.me>
2018-03-12 17:32:57 -1000

    Output index.html to project root. Simplified webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,42 @@
+const HTMLPlugin = require('html-webpack-plugin')
 const LicensePlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const pkg = require('./package.json');
 
-module.exports = merge(common.cfg, {
-  plugins: [
+// Determine if env is production
+const prod = process.argv.indexOf('-p') !== -1;
+
+// Get basename for generated file
+const shortidVersion = depVersion('shortid');
+
+// Configure webpack plugins
+const plugins = !prod ? []: [
     new LicensePlugin({
-      pattern: /.*/,
-      outputFilename: 'LICENSE.[name].txt'
+          pattern: /.*/,
+          outputFilename: 'LICENSE.[name].txt'
+        }),
+    new HTMLPlugin({
+        template: 'src/index.html',
+        filename: __dirname + '/index.html',
+        inject: false,
+        shortidVersion: shortidVersion,
+        cdnPrefix: 'https://unpkg.com/shortid-dist@' + pkg.version + '/dist/' + shortidVersion
     })
-  ]
-});
+];
+
+// Return package name (with version concatenated if defined in package.json dependencies)
+function depVersion(dep)
+{
+    var version = pkg.dependencies[dep];
+    return dep + (!version ? '' : ('-' + version.replace(/[^\d\.]/g, '')));
+}
+
+module.exports = {
+    entry: {
+        // license-webpack-plugin configuration expects entry to have a name 
+        'shortid': './index.js'
+    },
+    output: {
+        filename: shortidVersion + (prod ? '.min' : '') + '.js'
+    },
+    plugins: plugins
+};
